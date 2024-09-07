62b85156b4bef65e3082e487132317bc1f2394f2
Luixgustav0 <lgusret85@gmail.com>
2024-09-07 12:14:11 -0300

    Delete db.sql

--- db.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE usuario ;
-USE DATABASE usuario ;
-
-CREATE TABLE usuario (
-id (int) (10) not null auto_increment ,
-nome varchar (50) not null,
-email varchar (50) not null,
-idade varchar (50) not null,
-);
